443e0ad40e4f5a3e5e5e624195b8ab70c883d3a1
Joseph Worsfold <jworsf01@dcs.bbk.ac.uk>
2019-07-24 21:43:09 +0100

    Updated tests to eventually test real tweets. Wrote tests for the write back function.

--- test/test_twitter_api.py
+++ test/test_twitter_api.py
@@ -16,30 +16,18 @@ test_two = ['234123513245,0\n',
             '564918597520,1\n',
             '657456793278,1\n']
 
-test_three = ['1,0\n',
-              '2,1\n',
-              '3,0\n',
-              '4,0\n',
-              '5,1\n',
-              '6,0\n',
-              '7,1\n',
-              '8,0\n',
-              '9,0\n',
-              '10,1\n',
-              '11,0\n',
-              '12,1\n',
-              '13,0\n',
-              '14,0\n',
-              '15,0\n']
-
+test_real = ['896523232098078720,0,"No one is born hating another person because of the color of his skin or his background or his religion..."\n',
+             '896523304873238528,0,Thank you for everything. My last ask is the same as my first. I\'m asking you to believe—not in my ability to create change, but in yours.\n',
+             '796394920051441664,0,"To all the little girls watching...never doubt that you are valuable and powerful & deserving of every chance & opportunity in the world."\n']
 
 def test_read_tweet_ids():
     result_one = read_tweet_ids('test/test_data_one.txt')
     result_two = read_tweet_ids('test/test_data_two.txt')
-    result_three = read_tweet_ids('test/test_data_three.txt')
+    # will be used to test real tweets
+    # result_three = read_tweet_ids('test/test_data_real.txt')
     assert result_one == test_one
     assert result_two == test_two
-    assert result_three == test_three
+    #assert result_three == test_three
 
 
 def test_get_tweet_statuses():
@@ -47,4 +35,14 @@ def test_get_tweet_statuses():
 
 
 def test_write_tweet_status():
-    assert True
+    write_tweet_status('test/test_write_one.txt',test_one)
+    write_tweet_status('test/test_write_two.txt',test_two)
+    #write_tweet_status(test_three)
+
+    with open('test/test_write_one.txt') as test_file:
+        test_file_one = test_file.readlines()
+    with open('test/test_write_two.txt') as test_file:
+        test_file_two = test_file.readlines()
+
+    assert test_file_one == test_one
+    assert test_file_two == test_two
