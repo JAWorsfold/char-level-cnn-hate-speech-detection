565bd91ee9bfc3156b1dff8e4243115cebe6cf8e
Joseph Worsfold <jworsf01@dcs.bbk.ac.uk>
2019-08-10 18:21:39 +0100

    Created empty functions prior to creating tests. Reminded that I must deal with an unbalanced data set. Will try under-sampling, over-sampling and SMOTE.

--- src/svm_baseline_model.py
+++ src/svm_baseline_model.py
@@ -1,3 +1,6 @@
+"""This model is based partly on a similar model used by Thomas Davidson et al. in
+his paper: TBC"""
+
 import numpy as np
 import pandas as pd
 import nltk
@@ -5,13 +8,46 @@ import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 
-#I know I'm going to use a TfIdf Vectorizer so I'll build it here and write the functions after
-vectorizer = TfidfVectorizer(
-    preprocessor=preprocessor,
-    tokenizer=tokenizer,
-    stop_words=stopwords,
-    ngram_range=(1, 3),
-    decode_error='replace'
+def preprocessor(tweet):
+    """
+
+    :param tweet:
+    :return:
+    """
+    preprocessed_tweet = None
+    return preprocessed_tweet
+
+
+def tokenizer(tweet):
+    """
+
+    :param tweet:
+    :return:
+    """
+    tokenized_tweet = None
+    return tokenized_tweet
+
+
+def run_model ():
+    """
+    Decide whether to use over-sampling, under-sampling or SMOTE to deal with imbalance.
+    Class split: 0=25863, 1=2285
+    :return:
+    """
+
+
+    #I know I'm going to use a TfIdf Vectorizer so I'll build it here and write the functions after
+    vectorizer = TfidfVectorizer(
+        preprocessor=preprocessor,
+        tokenizer=tokenizer,
+        stop_words=stopwords,
+        ngram_range=(1, 3),
+        decode_error='replace'
     )
+
+
+if __name__ == '__main__':
+    run_model()
