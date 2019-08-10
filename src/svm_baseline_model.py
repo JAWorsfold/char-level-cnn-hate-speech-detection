681fb8e2483c4838ae9beab5c318a15c7642eb16
Joseph Worsfold <jworsf01@dcs.bbk.ac.uk>
2019-08-10 16:22:17 +0100

    Started work on basline SVM model. Preparing the tools I'll use when creating the model.

---
+++ src/svm_baseline_model.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import nltk
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import train_test_split
+
