95be75d533bae6546c1fc8bb8eab0213cd268820
Joseph Worsfold <jworsf01@dcs.bbk.ac.uk>
2019-08-10 16:51:52 +0100

    Going to use an SVM on Tf-Idf vectors and see if I can get a strong baseline. Will write and test my own preprocessing and tokenising function.

--- src/svm_baseline_model.py
+++ src/svm_baseline_model.py
@@ -6,3 +6,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
 from sklearn.model_selection import train_test_split
 
+
+#I know I'm going to use a TfIdf Vectorizer so I'll build it here and write the functions after
+vectorizer = TfidfVectorizer(
+    preprocessor=preprocessor,
+    tokenizer=tokenizer,
+    stop_words=stopwords,
+    ngram_range=(1, 3),
+    decode_error='replace'
+    )
