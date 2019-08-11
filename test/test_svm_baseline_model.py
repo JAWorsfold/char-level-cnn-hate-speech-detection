54e3b07406cbeef0c5a3deaec992cf26f3a4ccd6
Joseph Worsfold <jworsf01@dcs.bbk.ac.uk>
2019-08-11 09:14:08 +0100

    Preparing tests for preprocessor and tokenizer functions.

---
+++ test/test_svm_baseline_model.py
@@ -0,0 +1,28 @@
+from src.svm_baseline_model import *
+
+#test data
+#Need to know what the stemmer will return. Use test tweets (obama etc).
+
+input_one = "\"No one is born hating another person because of the color of his skin or his background or his religion...\" https://t.co/InZ58zkoAm"
+process_one = "\"No one is born hating another person because of the color of his skin or his background or his religion...\" https://t.co/InZ58zkoAm"
+token_one =
+
+input_two = "Our visit to @projectaware discussing the progress we made together and exploring future collaborations with @epa_kw highlighting its role in supporting environmental voluntary initiatives #guardiansofthesea https://www.instagram.com/p/B1A4NFCgbmo/?igshid=1vc2qn16t2spd"
+process_two =
+token_two =
+
+input_three = "Empty food dish.. pretty sure that look he is giving me is saying something.. you think @thechrisbarron? #Caturday"
+process_three =
+token_three =
+
+input_four = "TensorWatch: A debugging and visualization system for machine learning http://bit.ly/2KFUvqe #AI #DeepLearning #MachineLearning #DataScience"
+process_four =
+token_four =
+
+
+def test_preprocessor():
+    assert True
+
+
+def test_tokenizer():
+    assert True
