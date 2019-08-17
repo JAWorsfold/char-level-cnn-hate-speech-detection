ad935a481f06bb193ebc9c652cab22638a9ea345
Joseph Worsfold <jworsf01@dcs.bbk.ac.uk>
2019-08-17 20:23:32 +0100

    Creating one-hot-encodings for the restricted alphabet used in this basic CNN.

--- src/cnn_basic_model.py
+++ src/cnn_basic_model.py
@@ -10,10 +10,6 @@ from keras.layers import Conv1D, Activation, MaxPooling1D
 from keras.layers import Input, Embedding, Flatten, Dense
 from keras.models import Model
 
-# cnn parameters:
-VOCAB_SIZE = 70
-MAX_LEN = 140
-
 
 def cnn_preprocessor(tweet):
     """
@@ -21,6 +17,8 @@ def cnn_preprocessor(tweet):
     1) Mentions i.e. @someone
     2) URLs i.e. https://www.twitter.com
     3) Repeated whitespace
+    4) HTML entities
+    5) punctuation and numbers
     :param tweet: text string
     :return preprocessed_tweet: without mentions, URLs and extra whitespace
     """
@@ -39,7 +37,7 @@ def cnn_preprocessor(tweet):
 
 def cnn_tokenizer(tweet):
     """
-    Take a tweet - a simple text string - and remove punctuation, set characters to
+    Take a tweet - a simple text string - and set characters to
     lowercase and stem the tweets.
     :param tweet: a text string
     :return tokenized_tweet: all lowercase, no punctuation and stemmed
@@ -51,7 +49,7 @@ def cnn_tokenizer(tweet):
 
 
 def cnn_stopwords(tweet):
-
+    """Remove stopwords from a tweet"""
     stop_words = nltk.corpus.stopwords.words('english')
     other_words = ['rt', 'ff', 'tbt', 'ftw']  # may add more later
     stop_words.extend(other_words)
@@ -62,20 +60,45 @@ def cnn_stopwords(tweet):
 def train_model(data):
     """Train the CNN model"""
 
+
+
     tweet_df = pd.read_csv(data)
     tweet_df['tweet'] = tweet_df['tweet'].apply(lambda x: cnn_stopwords(x))
     tweet_df['tweet'] = tweet_df['tweet'].apply(lambda x: cnn_tokenizer(x))
     tweet_df['tweet'] = tweet_df['tweet'].apply(lambda x: cnn_preprocessor(x))
     y = tweet_df['class'].astype(int)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=33, test_size=0.1)
 
+    tokenizer = Tokenizer(char_level=True)
+    tokenizer.fit_on_texts(X_train)
+    alphabet = "abcdefghijklmnopqrstuvwxyz "
+    character_encoding = {}
+    for i, c in enumerate(alphabet):
+        character_encoding[c] = i + 1
+    tokenizer.word_index = character_encoding.copy()
 
+    # cnn parameters
+    max_len = 140
+    vocal_size = len(character_encoding)
 
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=33, test_size=0.1)
+    X_train = tokenizer.texts_to_sequences(X_train)
+    X_test  = tokenizer.texts_to_sequences(X_test)
+
+    X_train = pad_sequences(X_train, maxlen=max_len, padding='post')
+    X_test  = pad_sequences(X_test, maxlen=max_len, padding='post')
+
+    X_train = np.array(X_train, dtype=np.float32)
+    X_test  = np.array(X_test, dtype=np.float32)
+
 
     return
 
 
 if __name__ == '__main__':
-    train_model("data/private/td_zw_labeled_data.csv")
-    #text = "of course you were born in serbia...you're as fucked as A Serbian Film #MKR where's "
-    #print(cnn_tokenizer(text))
+    # train_model("data/private/td_zw_labeled_data.csv")
+
+    alphabet = "abcdefghijklmnopqrstuvwxyz "
+    character_encoding = {}
+    for i, c in enumerate(alphabet):
+        character_encoding[c] = i + 1
+    print(character_encoding)
