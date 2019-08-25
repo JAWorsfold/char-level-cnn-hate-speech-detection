daca5aefa1cada186dce493a6b6600fa235f2f24
Joseph Worsfold <jworsf01@dcs.bbk.ac.uk>
2019-08-25 20:31:27 +0100

    Refactoring preprocessing steps into a preprocessing Class to be used in multiple models during this project.

---
+++ src/preprocessing_utils.py
@@ -0,0 +1,29 @@
+"""
+
+"""
+class PreprocessUtils:
+
+    def __init__(self):
+        """"""
+        # define any necessary global variables
+        pass
+
+
+    def noise_removal(self):
+        """"""
+        pass
+
+
+    def normaliser(self):
+        """"""
+        pass
+
+
+    def tokenizer(self, as_list=False, ):
+        """"""
+        pass
+
+
+    def stopwords(self):
+        """"""
+        pass
