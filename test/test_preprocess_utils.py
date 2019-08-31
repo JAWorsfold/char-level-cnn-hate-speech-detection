de981189e6cc1d3816791ecb3e48320fa4fd1b8a
Joseph Worsfold <jworsf01@dcs.bbk.ac.uk>
2019-08-31 10:06:27 +0100

    Completed tests and confirmed they fail.

--- test/test_preprocess_utils.py
+++ test/test_preprocess_utils.py
@@ -18,14 +18,38 @@ def test_data():
 
 
 def test_remove_noise(pre_processor, test_data):
-    print(test_data)
-    assert True
+    with open('test/preprocess_data/noise_removed.csv') as expected:
+        exp_lst = list(csv.reader(expected))
+    for i in range(len(exp_lst)):
+        expected = exp_lst[i]
+        actual = pre_processor.remove_noise(test_data[i], mentions=True, replacement=' ')
+        assert actual == expected
 
 
-def test_normalise(pre_processor):
-    assert True
+def test_normalise_stopwords_false(pre_processor, test_data):
+    with open('test/preprocess_data/normalised.csv') as expected:
+        exp_lst = list(csv.reader(expected))
+    for i in range(len(exp_lst)):
+        expected = exp_lst[i]
+        actual = pre_processor.normalise(test_data[i], numbers=True, stopwords=False, replacement=' ')
+        assert actual == expected
 
 
-def test_tokenize(pre_processor):
-    assert True
+def test_normalise_stopwords_true(pre_processor, test_data):
+    with open('test/preprocess_data/normalised.csv') as expected:
+        exp_lst = list(csv.reader(expected))
+    add_stopwords = ['rt']
+    for i in range(len(exp_lst)):
+        expected = exp_lst[i]
+        actual = pre_processor.normalise(test_data[i], numbers=True, stopwords=True,
+                                         other_stopwords=add_stopwords, replacement=' ')
+        assert actual == expected
 
+
+def test_tokenize(pre_processor, test_data):
+    with open('test/preprocess_data/tokenized.csv') as expected:
+        exp_lst = list(csv.reader(expected))
+    for i in range(len(exp_lst)):
+        expected = exp_lst[i]
+        actual = pre_processor.tokenize(test_data[i])
+        assert ' '.join(actual) == expected
