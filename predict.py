356e3c52f5a824da23e5c0e035307f99a43d1fb3
Joseph Worsfold <jworsf01@dcs.bbk.ac.uk>
2019-09-14 21:41:27 +0100

    Final prediction program.

--- predict.py
+++ predict.py
@@ -1,9 +1,17 @@
 """Using the command line, run this file to classify user input as a file or string"""
 import argparse
 import csv
-from src.train_logreg import *
-from src.train_svm import *
-# from src.train_cnn import *
+import warnings
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore")
+    from src.train_cnn import *
+    from keras import models
+    from src.train_logreg import *
+    from src.train_svm import *
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 
 pp = PreProcessUtils()
@@ -31,15 +39,17 @@ def load_basic_model(model_file, vectorizer_file):
     """Load the either the Logistic Regression or SVM model and vectorizer"""
     load_model = pickle.load(open(model_file, 'rb'))
     load_vectorizer = pickle.load(open(vectorizer_file, 'rb'))
-    print("Loading model complete")
+    print("Loading model complete...")
     return load_model, load_vectorizer
 
 
 # basic or advanced passed as parameter
 def load_cnn(type):
     """Load either the normal CNN or advanced CNN and tokenizer"""
-    pass
-
+    load_model = models.load_model("models/" + type + ".keras")
+    load_tokenizer = pickle.load(open("models/" + type + "_tokenizer.pickle", 'rb'))
+    print("Loading model complete...")
+    return load_model, load_tokenizer
 
 def write_results(write_path, final_predictions):
     """Write back the results to a csv file"""
@@ -56,7 +66,7 @@ def run_prediction(args):
     pp_to_predict = list()     # preprocessed tweets for predictions
     final_predictions = list() # predictions and tweets for publishing
 
-    # try filepath
+    # try file path
     if args.filepath is not None:
         with open(args.filepath) as sample:
             sample_tweets = pd.read_csv(sample)
@@ -73,7 +83,22 @@ def run_prediction(args):
         raise Exception("No file path or string was provided in order to make a prediction")
 
     if args.model == 'cnn+' or args.model == 'cnn':
-        pass
+        model, tokenizer = load_cnn(args.model)
+        if args.model == 'cnn+':
+            for tweet in to_predict:
+                pp_to_predict.append(advanced_preprocess(tweet))
+        else:
+            for tweet in to_predict:
+                pp_to_predict.append(basic_preprocess(tweet))
+        print("Preprocessing complete...")
+        to_predict_tok = tokenizer.texts_to_sequences(pp_to_predict)
+        to_predict_tok = pad_sequences(to_predict_tok, maxlen=140, padding='post')
+        to_predict_tok = np.array(to_predict_tok, dtype=np.float32)
+
+        results = model.predict_classes(to_predict_tok)
+        results_prob = model.predict_proba(to_predict_tok)
+        for i in range(len(results)):
+            final_predictions.append([str(results[i]), str(results_prob[i]), str(to_predict[i])])
 
     elif args.model == 'svm' or args.model == 'logreg':
         model, vectorizer = load_basic_model("models/" + args.model + ".pickle",
@@ -99,7 +124,7 @@ def run_prediction(args):
     if args.write is not None:
         write_file_path = args.write + "/" + args.model + "_predictions.csv"
         written = write_results(write_file_path, final_predictions)
-        if written: print("Write back complete")
+        if written: print("Write back complete...")
 
     return True
 
